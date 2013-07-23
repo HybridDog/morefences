ae10d7732eb4be410cfc47e81c03f6669b5199d4
HybridDog <adresse_test@t-online.de>
2013-07-23 20:00:08 +0200

    update crafting

--- craftings.lua
+++ craftings.lua
@@ -1,267 +1,60 @@
+local function fencecraft(input, output)
 minetest.register_craft({
-	output = 'morefences:brick_fence',
+	output = "morefences:"..output.."_fence 6",
 	recipe = {
-		{'default:clay_brick', 'default:clay_brick', 'default:clay_brick'},
-		{'default:clay_brick', 'default:clay_brick', 'default:clay_brick'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:nc_rb_fence',
-	recipe = {
-		{'default:nyancat_rainbow', 'default:nyancat_rainbow', 'default:nyancat_rainbow'},
-		{'default:nyancat_rainbow', 'default:nyancat_rainbow', 'default:nyancat_rainbow'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:clay_fence',
-	recipe = {
-		{'default:clay', 'default:clay', 'default:clay'},
-		{'default:clay', 'default:clay', 'default:clay'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:stone_fence',
-	recipe = {
-		{'default:stone', 'default:stone', 'default:stone'},
-		{'default:stone', 'default:stone', 'default:stone'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:desert_stone_fence',
-	recipe = {
-		{'default:desert_stone', 'default:desert_stone', 'default:desert_stone'},
-		{'default:desert_stone', 'default:desert_stone', 'default:desert_stone'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:stonebrick_fence',
-	recipe = {
-		{'default:stonebrick', 'default:stonebrick', 'default:stonebrick'},
-		{'default:stonebrick', 'default:stonebrick', 'default:stonebrick'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:desert_stonebrick_fence',
-	recipe = {
-		{'default:desert_stonebrick', 'default:desert_stonebrick', 'default:desert_stonebrick'},
-		{'default:desert_stonebrick', 'default:desert_stonebrick', 'default:desert_stonebrick'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:dirt_fence',
-	recipe = {
-		{'default:dirt', 'default:dirt', 'default:dirt'},
-		{'default:dirt', 'default:dirt', 'default:dirt'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:sand_fence',
-	recipe = {
-		{'default:sand', 'default:sand', 'default:sand'},
-		{'default:sand', 'default:sand', 'default:sand'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:desert_sand_fence',
-	recipe = {
-		{'default:desert_sand', 'default:desert_sand', 'default:desert_sand'},
-		{'default:desert_sand', 'default:desert_sand', 'default:desert_sand'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:gravel_fence',
-	recipe = {
-		{'default:gravel', 'default:gravel', 'default:gravel'},
-		{'default:gravel', 'default:gravel', 'default:gravel'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:sandstone_fence',
-	recipe = {
-		{'default:sandstone', 'default:sandstone', 'default:sandstone'},
-		{'default:sandstone', 'default:sandstone', 'default:sandstone'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:sandstonebrick_fence',
-	recipe = {
-		{'default:sandstonebrick', 'default:sandstonebrick', 'default:sandstonebrick'},
-		{'default:sandstonebrick', 'default:sandstonebrick', 'default:sandstonebrick'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:cactus_fence',
-	recipe = {
-		{'default:cactus', 'default:cactus', 'default:cactus'},
-		{'default:cactus', 'default:cactus', 'default:cactus'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:cloud_fence',
-	recipe = {
-		{'default:cloud', 'default:cloud', 'default:cloud'},
-		{'default:cloud', 'default:cloud', 'default:cloud'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:cobble_fence',
-	recipe = {
-		{'default:cobble', 'default:cobble', 'default:cobble'},
-		{'default:cobble', 'default:cobble', 'default:cobble'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:mossycobble_fence',
-	recipe = {
-		{'default:mossycobble', 'default:mossycobble', 'default:mossycobble'},
-		{'default:mossycobble', 'default:mossycobble', 'default:mossycobble'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:coal_fence',
-	recipe = {
-		{'default:coal', 'default:coal', 'default:coal'},
-		{'default:coal', 'default:coal', 'default:coal'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:steel_fence',
-	recipe = {
-		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
-		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:copper_fence',
-	recipe = {
-		{'default:copper_ingot', 'default:copper_ingot', 'default:copper_ingot'},
-		{'default:copper_ingot', 'default:copper_ingot', 'default:copper_ingot'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:bronze_fence',
-	recipe = {
-		{'default:bronze_ingot', 'default:bronze_ingot', 'default:bronze_ingot'},
-		{'default:bronze_ingot', 'default:bronze_ingot', 'default:bronze_ingot'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:mese_fence',
-	recipe = {
-		{'default:mese_crystal_fragment', 'default:mese_crystal_fragment', 'default:mese_crystal_fragment'},
-		{'default:mese_crystal_fragment', 'default:mese_crystal_fragment', 'default:mese_crystal_fragment'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:gold_fence',
-	recipe = {
-		{'default:gold_ingot', 'default:gold_ingot', 'default:gold_ingot'},
-		{'default:gold_ingot', 'default:gold_ingot', 'default:gold_ingot'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:diamond_fence',
-	recipe = {
-		{'default:diamond', 'default:diamond', 'default:diamond'},
-		{'default:diamond', 'default:diamond', 'default:diamond'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:obsidian_fence',
-	recipe = {
-		{'default:obsidian_shard', 'default:obsidian_shard', 'default:obsidian_shard'},
-		{'default:obsidian_shard', 'default:obsidian_shard', 'default:obsidian_shard'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:nyancat_fence',
-	recipe = {
-		{'default:nyancat', 'default:nyancat', 'default:nyancat'},
-		{'default:nyancat', 'default:nyancat', 'default:nyancat'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:ice_fence',
-	recipe = {
-		{'default:ice', 'default:ice', 'default:ice'},
-		{'default:ice', 'default:ice', 'default:ice'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:snow_fence',
-	recipe = {
-		{'default:snow', 'default:snow', 'default:snow'},
-		{'default:snow', 'default:snow', 'default:snow'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:grass_fence',
-	recipe = {
-		{'default:junglegrass', 'default:junglegrass', 'default:junglegrass'},
-		{'default:junglegrass', 'default:junglegrass', 'default:junglegrass'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:jungleleave_fence',
-	recipe = {
-		{'default:jungleleaves', 'default:jungleleaves', 'default:jungleleaves'},
-		{'default:jungleleaves', 'default:jungleleaves', 'default:jungleleaves'},
-	}
-})
-
-minetest.register_craft({
-	output = 'morefences:leave_fence',
-	recipe = {
-		{'default:leaves', 'default:leaves', 'default:leaves'},
-		{'default:leaves', 'default:leaves', 'default:leaves'},
-	}
-})
-
-if (minetest.get_modpath("farming_plus")) then
-minetest.register_craft({
-	output = 'morefences:pumpkin_fence',
-	recipe = {
-		{'farming:pumpkin', 'farming:pumpkin', 'farming:pumpkin'},
-		{'farming:pumpkin', 'farming:pumpkin', 'farming:pumpkin'},
+		{input, input, input},
+		{input, input, input}
 	}
 })
 end
 
-if (minetest.get_modpath("moreores")) then
-minetest.register_craft({
-	output = 'morefences:mithril_fence',
-	recipe = {
-		{'moreores:mithril_ingot', 'moreores:mithril_ingot', 'moreores:mithril_ingot'},
-		{'moreores:mithril_ingot', 'moreores:mithril_ingot', 'moreores:mithril_ingot'},
-	}
-})
+local default_fence_list = {
+	{"clay_brick", "brick"},
+	{"nyancat_rainbow", "nc_rb"},
+	{"clay", "x"},
+	{"stone", "x"},
+	{"desert_stone", "x"},
+	{"stonebrick", "x"},
+	{"desert_stonebrick", "x"},
+	{"dirt", "x"},
+	{"sand", "x"},
+	{"desert_sand", "x"},
+	{"gravel", "x"},
+	{"sandstone", "x"},
+	{"sandstonebrick", "x"},
+	{"cactus", "x"},
+	{"cloud", "x"},
+	{"cobble", "x"},
+	{"mossycobble", "x"},
+	{"coal_lump", "x"},	--fixed
+	{"steel_ingot", "steel"},
+	{"copper_ingot", "copper"},
+	{"bronze_ingot", "bronze"},
+	{"mese_crystal_fragment", "mese"},
+	{"gold_ingot", "gold"},
+	{"diamond", "x"},
+	{"obsidian_shard", "obsidian"},
+	{"nyancat", "x"},
+	{"ice", "x"},
+	{"snow", "x"},
+	{"junglegrass", "grass"},
+	{"jungleleaves", "jungleleave"},	--remember to call them leaf, not leave
+	{"leaves", "leave"}
+}
+
+for _,i in ipairs(default_fence_list) do
+	local input = i[1]
+	local output = i[2]
+	if output == "x" then
+		output = input
+	end
+	fencecraft("default:"..input, output)
+end
+
+if minetest.get_modpath("farming_plus") then
+	fencecraft("farming:pumpkin", "pumpkin")
+end
+
+if minetest.get_modpath("moreores") then
+	fencecraft("moreores:mithril_ingot", "mithril")
 end
