c44d88c91c93e8c68dc7ae821b779f641cfd28cb
PenguinOfTheDeath <tamarok40@gmail.com>
2013-07-23 08:47:56 +0200

    Tidied up the code. Now using functions for registering fences.

--- fences.lua
+++ fences.lua
@@ -1,435 +1,86 @@
 do
-	local height = 0.5
-	local depth = -0.5
-	local other = 0.143
-	local other_minus = -0.143
-	minetest.register_node("morefences:brick_fence", {
-		description = "Brick Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_brick.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_brick.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
-
-	minetest.register_node("morefences:nc_rb_fence", {
-		description = "Nyancat Rainbow Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_nc_rb.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_nc_rb.png"},
-		groups = {cracky=2,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	local function fence(...)
+			name, description, texture = ...
+			minetest.register_node("morefences:"..name.."_fence", {
+					description = description.." Fence",
+					drawtype = "fencelike",
+					inventory_image = texture..".png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
+					paramtype = "light",
+					tiles = {texture..".png"},
+					groups = {cracky=3,fence=1},
+					selection_box = {
+							type = "fixed",
+							fixed = {-0.14285714285714, -0.5, -0.14285714285714, 0.14285714285714, 0.5, 0.14285714285714},
+					},
+			})
+	end
 	
-	minetest.register_node("morefences:clay_fence", {
-		description = "Clay Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_clay.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_clay.png"},
-		groups = {crumbly=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("brick", "Brick", "default_brick")
 	
-	minetest.register_node("morefences:stone_fence", {
-		description = "Stone Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_stone.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_stone.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("nc_rb", "Nyancat Rainbow", "default_nc_rb")
 	
-	minetest.register_node("morefences:desert_stone_fence", {
-		description = "Desert Stone Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_desert_stone.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_desert_stone.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("clay", "Clay", "default_clay")
 	
-	minetest.register_node("morefences:stonebrick_fence", {
-		description = "Stone Brick Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_stone_brick.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_stone_brick.png"},
-		groups = {cracky=2,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("stone", "Stone", "default_stone")
 	
-	minetest.register_node("morefences:desert_stonebrick_fence", {
-		description = "Desert Stone Brick Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_desert_stone_brick.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_desert_stone_brick.png"},
-		groups = {cracky=2,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("desert_stone", "Desert Stone", "default_desert_stone")
+		
+	fence("stonebrick", "Stone Brick", "default_stone_brick")
 	
-	minetest.register_node("morefences:dirt_fence", {
-		description = "Dirt Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_dirt.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_dirt.png"},
-		groups = {crumbly=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("brick", "Brick", "default_brick")
 	
-	minetest.register_node("morefences:sand_fence", {
-		description = "Sand Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_sand.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_sand.png"},
-		groups = {crumbly=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("desert_stonebrick", "Desert Stone Brick", "default_desert_stone_brick")
+		
+	fence("dirt", "Dirt", "default_dirt")
 	
-	minetest.register_node("morefences:desert_sand_fence", {
-		description = "Desert Sand Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_desert_sand.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_desert_sand.png"},
-		groups = {crumbly=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("sand", "Sand", "default_sand")
 	
-	minetest.register_node("morefences:gravel_fence", {
-		description = "Gravel Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_gravel.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_gravel.png"},
-		groups = {crumbly=2,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("desert_sand", "Desert Sand", "default_desert_sand")
 	
-	minetest.register_node("morefences:sandstone_fence", {
-		description = "Sandstone Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_sandstone.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_sandstone.png"},
-		groups = {cracky=3,crumbly=2,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("gravel", "Gravel", "default_gravel")
 	
-	minetest.register_node("morefences:sandstonebrick_fence", {
-		description = "Sandstone Brick Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_sandstone_brick.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_sandstone_brick.png"},
-		groups = {cracky=2,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("sandstone", "Sandstone", "default_sandstone")
 	
-	minetest.register_node("morefences:cactus_fence", {
-		description = "Cactus Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_cactus_side.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_cactus_side.png"},
-		groups = {snappy=1,choppy=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("sandstonebrick", "Sandstone Brick", "default_sandstone_brick")
 	
-	minetest.register_node("morefences:cloud_fence", {
-		description = "Cloud Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_cloud.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_cloud.png"},
-		groups = {cracky=1,level=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("cactus", "Cactus", "default_cactus_side")
 	
-	minetest.register_node("morefences:cobble_fence", {
-		description = "Cobble Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_cobble.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_cobble.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("cloud", "Cloud", "default_cloud")
 	
-	minetest.register_node("morefences:mossycobble_fence", {
-		description = "Mossycobble Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_mossycobble.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_mossycobble.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("cobble", "Cobble", "default_cobble")
 	
-	minetest.register_node("morefences:coal_fence", {
-		description = "Coal Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_coal_block.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_coal_block.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("mossycobble", "Mossycobble", "default_mossycobble")
 	
-	minetest.register_node("morefences:steel_fence", {
-		description = "Steel Fence",
-		drawtype = "fencelike",
-		inventory_image = "morefences_steel.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"morefences_steel.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("coal", "Coal", "default_coal_block")
 	
-	minetest.register_node("morefences:copper_fence", {
-		description = "Copper Fence",
-		drawtype = "fencelike",
-		inventory_image = "morefences_copper.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"morefences_copper.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("steel", "Steel", "morefences_steel")
 	
-	minetest.register_node("morefences:bronze_fence", {
-		description = "Bronze Fence",
-		drawtype = "fencelike",
-		inventory_image = "morefences_bronze.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"morefences_bronze.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("copper", "Copper", "morefences_copper")
 	
-	minetest.register_node("morefences:mese_fence", {
-		description = "MESE Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_mese_block.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_mese_block.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("bronze", "Bronze", "morefences_bronze")
 	
-	minetest.register_node("morefences:gold_fence", {
-		description = "Gold Fence",
-		drawtype = "fencelike",
-		inventory_image = "morefences_gold.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"morefences_gold.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("mese", "MESE", "default_mese_block")
 	
-	minetest.register_node("morefences:diamond_fence", {
-		description = "Diamond Fence",
-		drawtype = "fencelike",
-		inventory_image = "morefences_diamond.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"morefences_diamond.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("gold", "Gold", "morefences_gold")
 	
-	minetest.register_node("morefences:obsidian_fence", {
-		description = "Obsidian Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_obsidian.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_obsidian.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("diamond", "Diamond", "morefences_diamond")
 	
-	minetest.register_node("morefences:nyancat_fence", {
-		description = "Nyancat Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_nc_side.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_nc_side.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("obsidian", "Obsidian", "default_obsidian")
 	
-	minetest.register_node("morefences:ice_fence", {
-		description = "Ice Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_ice.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_ice.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("nyancat", "Nyancat", "default_nc_side")
 	
-	minetest.register_node("morefences:snow_fence", {
-		description = "Snow Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_snow.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_snow.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("ice", "Ice", "default_ice")
 	
-	minetest.register_node("morefences:grass_fence", {
-		description = "Grass Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_grass.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_grass.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("snow", "Snow", "default_snow")
 	
-	minetest.register_node("morefences:pumpkin_fence", {
-		description = "Pumpkin Fence",
-		drawtype = "fencelike",
-		inventory_image = "morefences_pumpkin.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"morefences_pumpkin.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("grass", "Grass", "default_grass")
 	
-	minetest.register_node("morefences:leave_fence", {
-		description = "Leave Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_leaves.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_leaves.png"},
-		groups = {snappy=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("leave", "Leave", "default_leaves")
 	
-	minetest.register_node("morefences:jungleleave_fence", {
-		description = "Jungle Leave Fence",
-		drawtype = "fencelike",
-		inventory_image = "default_jungleleaves.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"default_jungleleaves.png"},
-		groups = {snappy=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("jungleleave", "Jungle Leave", "default_jungleleaves")
 	
-	minetest.register_node("morefences:mithril_fence", {
-		description = "Mithril Fence",
-		drawtype = "fencelike",
-		inventory_image = "morefences_mithril.png^morefences_fn.png^[makealpha:255,126,126^morefences_sh.png",
-		paramtype = "light",
-		tiles = {"morefences_mithril.png"},
-		groups = {cracky=3,fence=1},
-		selection_box = {
-			type = "fixed",
-			fixed = {other_minus, depth, other_minus, other, height, other},
-		},
-	})
+	fence("pumpkin", "Pumpkin", "morefences_pumpkin")
+	
+	fence("mithril", "Mithril", "morefences_mithril")
 
 end
